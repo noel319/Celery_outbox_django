135ed0e433774a19d4c611af9aeade5af33fe2ec
noel <azeisl917@gmail.com>
2024-11-11 14:48:03 +0300

    update settings

--- src/core/settings.py
+++ src/core/settings.py
@@ -30,6 +30,7 @@ INSTALLED_APPS = [
 
     # project apps
     'users',
+    'core'
 ]
 
 MIDDLEWARE = [
@@ -110,7 +111,7 @@ STATIC_ROOT = env("STATIC_ROOT")
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-CELERY_BROKER = env("CELERY_BROKER", default="redis://localhost:6379/0")
+CELERY_BROKER = env("CELERY_BROKER", default="redis://redis:6379/0")
 CELERY_ALWAYS_EAGER = env("CELERY_ALWAYS_EAGER", default=DEBUG)
 
 LOG_FORMATTER = env("LOG_FORMATTER", default="console")
